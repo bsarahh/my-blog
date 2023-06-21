9078b6fc87308a2a5525fe883cfc9b3569252a2c
Bouchmal Sarah <bouchmal.sarah7@gmail.com>
2023-06-21 11:41:50 +0100

    Add gunicorne

--- forms.py
+++ forms.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
-##WTForm
+# WTForm
+
+
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+
 
 class LeaveComment(FlaskForm):
     comment_space = CKEditorField("Comment", validators=[DataRequired()])
